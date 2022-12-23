65ee2b97e10015c46e175e11e45e0b3aea48727b
Fernando Perez <Fernando.Perez@berkeley.edu>
2022-12-23 18:01:58 -0500

    Add proper package __init__ file

---
+++ jupytee/__init__.py
@@ -0,0 +1,16 @@
+"""Magics to support GPT-3 interactions in IPython/Jupyter.
+"""
+
+from .jupytee import GPTMagics
+
+
+__version__ = "0.0.1"
+
+
+def load_ipython_extension(ipython):
+    """
+    Any module file that define a function named `load_ipython_extension`
+    can be loaded via `%load_ext module.path` or be configured to be
+    autoloaded by IPython at startup time.
+    """
+    ipython.register_magics(GPTMagics)
